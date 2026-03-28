9fb384e187fa685a19019434cc42adc96d62fe8a
EshwarAkhilesh25 <battulaeshwarakhilesh@gmail.com>
2026-03-28 17:00:53 +0530

    fix: point frontend to GCP Nakama server

--- frontend/src/utils/nakamaClient.ts
+++ frontend/src/utils/nakamaClient.ts
@@ -1,5 +1,9 @@
 import { Client } from "@heroiclabs/nakama-js";
 
-const client = new Client("defaultkey", "localhost", "7350", false);
+const SERVER_HOST = process.env.REACT_APP_NAKAMA_HOST || "136.111.140.22";
+const SERVER_PORT = process.env.REACT_APP_NAKAMA_PORT || "7350";
+const USE_SSL = process.env.REACT_APP_NAKAMA_SSL === "true";
+
+const client = new Client("defaultkey", SERVER_HOST, SERVER_PORT, USE_SSL);
 
 export default client;
